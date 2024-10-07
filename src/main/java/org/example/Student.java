28f5cf6623a2e261697cf37c85bed9f9f12a1158
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2024-10-07 09:50:30 +0100

    setup

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String email;
+
+    private String course;
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                '}';
+    }
+
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public Student(){
+        this.name = "";
+        this.email = "";
+        this.course = "";
+    }
+}
