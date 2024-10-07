fe867a8ed5b1ebc2303af1ffd05ae410b3fa59d7
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2024-10-07 09:45:10 +0100

    setup

---
+++ src/main/java/org/example/StudentApp.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class StudentApp {
+    public static void main(String[] args){
+        //Prompt user for details
+        Student firstStudent = new Student();
+        firstStudent.setName("");
+        firstStudent.setEmail("");
+        firstStudent.setCourse("");
+
+        System.out.println("Details are "+ firstStudent.toString());
+
+        Student secondStudent = new Student("joe", "joe.123", "civil");
+    }
+
+}
